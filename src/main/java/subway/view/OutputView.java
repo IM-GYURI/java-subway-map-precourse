903babefc3cea371310af8c8aeff92e343b2accc
IM-GYURI <kgulr0517@gmail.com>
2024-11-19 06:37:09 +0900

    feat : OutputView에 역 관리 화면, 역 등록, 역 삭제, 역 조회 출력을 구현한다.
    - printStationManagement() : 역 관리 화면 출력
    - printEnrollStationSuccess() : 역 등록 성공 메세지 출력
    - printDeleteStationSuccess() : 역 등록 삭제 메세지 출력
    - printStations() : 모든 역 목록 조회
    - formatStation() : 역 이름을 출력 문구로 포맷팅

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -2,6 +2,8 @@ package subway.view;
 
 import java.util.List;
 import subway.domain.Line;
+import subway.domain.Station;
+import subway.repository.StationRepository;
 
 public class OutputView {
 
@@ -14,16 +16,54 @@ public class OutputView {
         System.out.println(MainFeature.QUIT.message + ". 종료" + System.lineSeparator());
     }
 
+    public void printStationManagement() {
+        System.out.println(System.lineSeparator()
+                + Sentence.PREFIX.message + "역 관리 화면");
+        System.out.println(StationFeature.SELECT_ONE.message + ". 역 등록");
+        System.out.println(StationFeature.SELECT_TWO.message + ". 역 삭제");
+        System.out.println(StationFeature.SELECT_THREE.message + ". 역 조회");
+        System.out.println(StationFeature.BACK.message + ". 돌아가기" + System.lineSeparator());
+    }
+
+    public void printEnrollStationSuccess() {
+        System.out.println(System.lineSeparator()
+                + Sentence.INFO.message + "지하철 역이 등록되었습니다.");
+    }
+
+    public void printDeleteStationSuccess() {
+        System.out.println(System.lineSeparator()
+                + Sentence.INFO.message + "지하철 역이 삭제되었습니다.");
+    }
+
+    public void printStations() {
+        List<Station> stations = StationRepository.stations();
+        StringBuilder sentences = new StringBuilder();
+        stations.forEach(station -> {
+            sentences.append(formatStation(station))
+                    .append(System.lineSeparator());
+        });
+
+        System.out.println(System.lineSeparator()
+                + Sentence.PREFIX.message + "역 목록");
+        System.out.print(sentences);
+    }
+
     public void printSubwayLinesInformation(List<Line> lines) {
-        System.out.println(System.lineSeparator() + Sentence.PREFIX.message + "지하철 노선도");
+        System.out.println(System.lineSeparator()
+                + Sentence.PREFIX.message + "지하철 노선도");
         lines.forEach(line -> {
             String sentence = formatLineAndStations(line);
-            System.out.println(sentence + System.lineSeparator());
+            System.out.println(sentence);
         });
     }
 
     public static void printError(String errorMessage) {
-        System.out.println(System.lineSeparator() + errorMessage + System.lineSeparator());
+        System.out.println(System.lineSeparator()
+                + errorMessage + System.lineSeparator());
+    }
+
+    private String formatStation(Station station) {
+        return Sentence.INFO.message + station.getName();
     }
 
     private String formatLineAndStations(Line line) {
