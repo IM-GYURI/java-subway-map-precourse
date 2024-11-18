601330f867912ec3caa5cd5249f82dd2aa1418dd
IM-GYURI <kgulr0517@gmail.com>
2024-11-19 01:52:10 +0900

    feat : 빌더 패턴을 적용한 검증 로직을 구현한다.

---
+++ src/main/java/subway/exception/ValidatorBuilder.java
@@ -0,0 +1,49 @@
+package subway.exception;
+
+import java.util.function.Predicate;
+
+public class ValidatorBuilder<T> {
+    private final T value;
+    private int numericValue;
+
+    private ValidatorBuilder(final T Value) {
+        this.value = Value;
+    }
+
+    public static <T> ValidatorBuilder<T> from(final T Value) {
+        return new ValidatorBuilder<T>(Value);
+    }
+
+    public ValidatorBuilder<T> validate(final Predicate<T> condition, final ErrorMessage errorMessage) {
+        if (condition.test(value)) {
+            throw new IllegalArgumentException(errorMessage.toString());
+        }
+
+        return this;
+    }
+
+    public ValidatorBuilder<T> validateInteger(final Predicate<Integer> condition, final ErrorMessage errorMessage) {
+        if (condition.test(numericValue)) {
+            throw new IllegalArgumentException(errorMessage.toString());
+        }
+
+        return this;
+    }
+
+    public ValidatorBuilder<T> validateIsInteger() {
+        try {
+            numericValue = Integer.parseInt(value.toString());
+            return this;
+        } catch (NumberFormatException e) {
+            throw e;
+        }
+    }
+
+    public T get() {
+        return value;
+    }
+
+    public int getNumericValue() {
+        return numericValue;
+    }
+}
