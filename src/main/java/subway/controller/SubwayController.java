442c8deba4f3bfaa30cd12da514b4ab7fda84797
IM-GYURI <kgulr0517@gmail.com>
2024-11-19 06:43:01 +0900

    feat : SubwayController에 역 관리 기능을 구현한다.
    - processSelectFeature() -> handledMain() 함수명 변경
    - selectMainFeature() 내부에 검증 로직 추가
    - handledMain() : 메인 화면의 1번 선택 시 로직 구현
    - selectStationFeature() : 역 관리 화면에서 사용자 입력을 처리
    - processStationManagement() : 역 관리 화면의 선택지들 구현
    - enrollStation() : 역 등록
    - deleteStation() : 역 삭제
    - showStations() : 역 조회

--- src/main/java/subway/controller/SubwayController.java
+++ src/main/java/subway/controller/SubwayController.java
@@ -3,12 +3,16 @@ package subway.controller;
 import java.util.EnumMap;
 import java.util.List;
 import subway.domain.Line;
+import subway.domain.Station;
+import subway.exception.InputValidator;
 import subway.repository.LineRepository;
+import subway.repository.StationRepository;
 import subway.util.FileLoader;
 import subway.util.RetryHandler;
 import subway.view.InputView;
 import subway.view.MainFeature;
 import subway.view.OutputView;
+import subway.view.StationFeature;
 
 public class SubwayController {
 
@@ -26,20 +30,24 @@ public class SubwayController {
         FileLoader.loadSettings();
 
         while (isRunning) {
-            processSelectFeature(getSelectFeature());
+            handledMain(selectMainFeature());
+            System.out.println();
         }
     }
 
-    private MainFeature getSelectFeature() {
+    private MainFeature selectMainFeature() {
         outputView.printMain();
-        return RetryHandler.getValidInput(() -> {
+        return RetryHandler.handleRetry(() -> {
             String selectedFeature = inputView.askFeature();
+            InputValidator.validateInput(selectedFeature);
+
             return MainFeature.getFeatureFromInput(selectedFeature);
         });
     }
 
-    private void processSelectFeature(MainFeature selectedFeature) {
+    private void handledMain(MainFeature selectedFeature) {
         EnumMap<MainFeature, Runnable> actions = new EnumMap<>(MainFeature.class);
+        actions.put(MainFeature.SELECT_ONE, () -> processStationManagement(selectStationFeature()));
         actions.put(MainFeature.SELECT_FOUR, this::showSubwayLines);
         actions.put(MainFeature.QUIT, this::quitProgram);
 
@@ -47,6 +55,56 @@ public class SubwayController {
         if (action != null) {
             action.run();
         }
+    }
+
+    private StationFeature selectStationFeature() {
+        outputView.printStationManagement();
+        return RetryHandler.handleRetry(() -> {
+            String selectedFeature = inputView.askFeature();
+            InputValidator.validateInput(selectedFeature);
+
+            return StationFeature.getFeatureFromInput(selectedFeature);
+        });
+    }
+
+    private void processStationManagement(StationFeature selectedFeature) {
+        EnumMap<StationFeature, Runnable> actions = new EnumMap<>(StationFeature.class);
+        actions.put(StationFeature.SELECT_ONE, this::enrollStation);
+        actions.put(StationFeature.SELECT_TWO, this::deleteStation);
+        actions.put(StationFeature.SELECT_THREE, this::showStations);
+        actions.put(StationFeature.BACK, () -> {
+        });
+
+        Runnable action = actions.get(selectedFeature);
+        if (action != null) {
+            action.run();
+        }
+    }
+
+    private void enrollStation() {
+        RetryHandler.handleRetry(() -> {
+            String stationName = inputView.askStationEnroll();
+            InputValidator.validateInput(stationName);
+            InputValidator.validateNameLength(stationName);
+
+            StationRepository.addStation(new Station(stationName));
+            outputView.printEnrollStationSuccess();
+        });
+    }
+
+    private void deleteStation() {
+        RetryHandler.handleRetry(() -> {
+            String stationName = inputView.askStationDelete();
+            InputValidator.validateInput(stationName);
+            InputValidator.validateNameLength(stationName);
+
+            StationRepository.deleteStation(stationName);
+            outputView.printDeleteStationSuccess();
+        });
+    }
+
+    private void showStations() {
+        outputView.printStations();
     }
 
     private void showSubwayLines() {
