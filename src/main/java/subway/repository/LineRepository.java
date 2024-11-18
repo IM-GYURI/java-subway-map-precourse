8018b4b672a808b4b1425a650daa29d8ebb13744
IM-GYURI <kgulr0517@gmail.com>
2024-11-19 01:47:01 +0900

    feat : 노선을 추가할 시, 이미 존재하는 노선 이름이 있는지 검증한 후 추가하도록 로직을 수정한다.

--- src/main/java/subway/repository/LineRepository.java
+++ src/main/java/subway/repository/LineRepository.java
@@ -1,9 +1,11 @@
-package subway.domain;
+package subway.repository;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import subway.domain.Line;
+import subway.exception.ErrorMessage;
 
 public class LineRepository {
     private static final List<Line> lines = new ArrayList<>();
@@ -13,10 +15,19 @@ public class LineRepository {
     }
 
     public static void addLine(Line line) {
+        if (isDuplicate(line.getName())) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_LINE_NAME.toString());
+        }
+        
         lines.add(line);
     }
 
     public static boolean deleteLineByName(String name) {
         return lines.removeIf(line -> Objects.equals(line.getName(), name));
+    }
+
+    private static boolean isDuplicate(String name) {
+        return lines.stream()
+                .anyMatch(station -> Objects.equals(station.getName(), name));
     }
 }
