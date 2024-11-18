ae7a66cba448b4f02959b3c41aedc74eb4c9dba7
IM-GYURI <kgulr0517@gmail.com>
2024-11-19 06:38:45 +0900

    feat : RetryHandler가 Supplier와 Runnable을 둘 다 처리할 수 있도록 구현한다.
    - 오버로딩
    - 함수명 수정 : getValidInput -> handleRetry

--- src/main/java/subway/util/RetryHandler.java
+++ src/main/java/subway/util/RetryHandler.java
@@ -7,11 +7,25 @@ import subway.view.OutputView;
 public class RetryHandler {
     private static final int MAX_ATTEMPTS = 5;
 
-    public static <T> T getValidInput(final Supplier<T> supplier) {
+    public static <T> T handleRetry(final Supplier<T> supplier) {
         int attempts = 0;
         while (attempts < MAX_ATTEMPTS) {
             try {
                 return supplier.get();
+            } catch (IllegalArgumentException e) {
+                attempts++;
+                OutputView.printError(e.getMessage());
+            }
+        }
+        throw new IllegalArgumentException(ErrorMessage.TOO_MANY_INVALID_INPUT.toString());
+    }
+
+    public static void handleRetry(final Runnable runnable) {
+        int attempts = 0;
+        while (attempts < MAX_ATTEMPTS) {
+            try {
+                runnable.run();
+                return;
             } catch (IllegalArgumentException e) {
                 attempts++;
                 OutputView.printError(e.getMessage());
