e2bb5c4455c8fb7a37ffc5f01fc2c4fa6f39c881
IM-GYURI <kgulr0517@gmail.com>
2024-11-19 06:35:03 +0900

    feat : InputView에 역 등록과 역 삭제를 묻는 기능을 구현한다.

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -14,4 +14,16 @@ public class InputView {
         System.out.println(Sentence.PREFIX.message + "원하는 기능을 선택하세요.");
         return scanner.nextLine().trim();
     }
+
+    public String askStationEnroll() {
+        System.out.println(System.lineSeparator()
+                + Sentence.PREFIX.message + "등록할 역 이름을 입력하세요.");
+        return scanner.nextLine().trim();
+    }
+
+    public String askStationDelete() {
+        System.out.println(System.lineSeparator()
+                + Sentence.PREFIX.message + "삭제할 역 이름을 입력하세요.");
+        return scanner.nextLine().trim();
+    }
 }
