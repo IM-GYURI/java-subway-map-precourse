c3124afa86b134f3c9a8c4ed9c2fe09eed039ad6
IM-GYURI <kgulr0517@gmail.com>
2024-11-19 10:42:35 +0900

    feat : LineRepository에 노선 이름으로 노선 객체를 찾아 반환하는 로직을 구현한다.
    - deleteLineByName() : 존재하는 노선 이름인지 검증 추가
    - findLine() : Optional<Line> 반환

--- src/main/java/subway/repository/LineRepository.java
+++ src/main/java/subway/repository/LineRepository.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import subway.domain.Line;
 import subway.exception.ErrorMessage;
 
@@ -18,12 +19,22 @@ public class LineRepository {
         if (isDuplicate(line.getName())) {
             throw new IllegalArgumentException(ErrorMessage.DUPLICATE_LINE_NAME.toString());
         }
-        
+
         lines.add(line);
     }
 
-    public static boolean deleteLineByName(String name) {
-        return lines.removeIf(line -> Objects.equals(line.getName(), name));
+    public static void deleteLineByName(String name) {
+        if (findLine(name).isEmpty()) {
+            throw new IllegalArgumentException(ErrorMessage.LINE_NOT_EXISTS.toString());
+        }
+
+        lines.removeIf(line -> Objects.equals(line.getName(), name));
+    }
+
+    public static Optional<Line> findLine(String name) {
+        return lines.stream()
+                .filter(line -> line.getName().equals(name))
+                .findFirst();
     }
 
     private static boolean isDuplicate(String name) {
