e9982687b1b529e58ed753a3903398b995edbffe
IM-GYURI <kgulr0517@gmail.com>
2024-11-19 10:39:48 +0900

    feat : ErrorMessage를 추가한다.
    - LINE_NOT_EXISTS("존재하지 않는 노선 이름입니다.")

--- src/main/java/subway/exception/ErrorMessage.java
+++ src/main/java/subway/exception/ErrorMessage.java
@@ -6,6 +6,7 @@ public enum ErrorMessage {
     INVALID_NAME_LENGTH("이름은 2글자 이상이어야 합니다."),
     DUPLICATE_STATION_NAME("이미 존재하는 역 이름입니다."),
     STATION_NOT_EXISTS("존재하지 않는 역 이름입니다."),
+    LINE_NOT_EXISTS("존재하지 않는 노선 이름입니다."),
     DUPLICATE_LINE_NAME("이미 존재하는 노선 이름입니다."),
     STATIONS_SHORTAGE("노선은 2개 이상의 역을 포함해야 합니다."),
     INVALID_FEATURE("선택할 수 없는 기능입니다."),
