bfd982b8479808e5915b340e8689b49a4879eb24
IM-GYURI <kgulr0517@gmail.com>
2024-11-19 14:56:36 +0900

    feat : AppConfig에 서비스들을 추가한다.
    - LineService
    - MainService
    - SectionService
    - StationService

--- src/main/java/subway/config/AppConfig.java
+++ src/main/java/subway/config/AppConfig.java
@@ -2,6 +2,10 @@ package subway.config;
 
 import java.util.Scanner;
 import subway.controller.SubwayController;
+import subway.service.LineService;
+import subway.service.MainService;
+import subway.service.SectionService;
+import subway.service.StationService;
 import subway.view.InputView;
 import subway.view.OutputView;
 
@@ -12,7 +16,12 @@ public enum AppConfig {
     private final Scanner scanner = new Scanner(System.in);
     private final InputView inputView = new InputView(scanner);
     private final OutputView outputView = new OutputView();
-    private final SubwayController subwayController = new SubwayController(inputView, outputView);
+    private final MainService mainService = new MainService();
+    private final StationService stationService = new StationService();
+    private final LineService lineService = new LineService();
+    private final SectionService sectionService = new SectionService();
+    private final SubwayController subwayController = new SubwayController(inputView, outputView, mainService,
+            stationService, lineService, sectionService);
 
     public SubwayController getController() {
         return subwayController;
