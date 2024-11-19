00adc99fa7fbde7054887e7a72cfb5eb8a7a7c9a
IM-GYURI <kgulr0517@gmail.com>
2024-11-19 23:26:35 +0900

    refactor : PREFIX와 INFO를 상수로 만들어 사용하도록 수정한다.

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -2,10 +2,16 @@ package subway.view;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import subway.constants.MainFeature;
+import subway.constants.SectionFeature;
+import subway.constants.StationLineFeature;
 import subway.domain.Line;
 import subway.domain.Station;
 
 public class OutputView {
+
+    private static final String PREFIX = "## ";
+    private static final String INFO = "[INFO] ";
 
     public static void printError(String errorMessage) {
         System.out.println(System.lineSeparator()
@@ -17,31 +23,31 @@ public class OutputView {
     }
 
     public void printMain() {
-        System.out.println(Sentence.PREFIX.message + "메인 화면");
-        System.out.println(MainFeature.SELECT_ONE.message + ". 역 관리");
-        System.out.println(MainFeature.SELECT_TWO.message + ". 노선 관리");
-        System.out.println(MainFeature.SELECT_THREE.message + ". 구간 관리");
-        System.out.println(MainFeature.SELECT_FOUR.message + ". 지하철 노선도 출력");
-        System.out.println(MainFeature.QUIT.message + ". 종료" + System.lineSeparator());
+        System.out.println(PREFIX + "메인 화면");
+        System.out.println(MainFeature.SELECT_ONE.toString() + ". 역 관리");
+        System.out.println(MainFeature.SELECT_TWO.toString() + ". 노선 관리");
+        System.out.println(MainFeature.SELECT_THREE.toString() + ". 구간 관리");
+        System.out.println(MainFeature.SELECT_FOUR.toString() + ". 지하철 노선도 출력");
+        System.out.println(MainFeature.QUIT.toString() + ". 종료" + System.lineSeparator());
     }
 
     public void printStationManagement() {
         System.out.println(System.lineSeparator()
-                + Sentence.PREFIX.message + "역 관리 화면");
-        System.out.println(StationLineFeature.SELECT_ONE.message + ". 역 등록");
-        System.out.println(StationLineFeature.SELECT_TWO.message + ". 역 삭제");
-        System.out.println(StationLineFeature.SELECT_THREE.message + ". 역 조회");
-        System.out.println(StationLineFeature.BACK.message + ". 돌아가기" + System.lineSeparator());
+                + PREFIX + "역 관리 화면");
+        System.out.println(StationLineFeature.SELECT_ONE.toString() + ". 역 등록");
+        System.out.println(StationLineFeature.SELECT_TWO.toString() + ". 역 삭제");
+        System.out.println(StationLineFeature.SELECT_THREE.toString() + ". 역 조회");
+        System.out.println(StationLineFeature.BACK.toString() + ". 돌아가기" + System.lineSeparator());
     }
 
     public void printEnrollStationSuccess() {
         System.out.println(System.lineSeparator()
-                + Sentence.INFO.message + "지하철 역이 등록되었습니다.");
+                + INFO + "지하철 역이 등록되었습니다.");
     }
 
     public void printDeleteStationSuccess() {
         System.out.println(System.lineSeparator()
-                + Sentence.INFO.message + "지하철 역이 삭제되었습니다.");
+                + INFO + "지하철 역이 삭제되었습니다.");
     }
 
     public void printStations(List<Station> stations) {
@@ -52,13 +58,13 @@ public class OutputView {
         });
 
         System.out.println(System.lineSeparator()
-                + Sentence.PREFIX.message + "역 목록");
+                + PREFIX + "역 목록");
         System.out.print(sentences);
     }
 
     public void printSubwayLinesInformation(List<Line> lines) {
         System.out.println(System.lineSeparator()
-                + Sentence.PREFIX.message + "지하철 노선도");
+                + PREFIX + "지하철 노선도");
         String result = lines.stream()
                 .map(this::formatLineAndStations)
                 .collect(Collectors.joining(System.lineSeparator().repeat(2)));
@@ -67,26 +73,26 @@ public class OutputView {
 
     public void printLineManagement() {
         System.out.println(System.lineSeparator()
-                + Sentence.PREFIX.message + "노선 관리 화면");
-        System.out.println(StationLineFeature.SELECT_ONE.message + ". 노선 등록");
-        System.out.println(StationLineFeature.SELECT_TWO.message + ". 노선 삭제");
-        System.out.println(StationLineFeature.SELECT_THREE.message + ". 노선 조회");
-        System.out.println(StationLineFeature.BACK.message + ". 돌아가기" + System.lineSeparator());
+                + PREFIX + "노선 관리 화면");
+        System.out.println(StationLineFeature.SELECT_ONE.toString() + ". 노선 등록");
+        System.out.println(StationLineFeature.SELECT_TWO.toString() + ". 노선 삭제");
+        System.out.println(StationLineFeature.SELECT_THREE.toString() + ". 노선 조회");
+        System.out.println(StationLineFeature.BACK.toString() + ". 돌아가기" + System.lineSeparator());
     }
 
     public void printEnrollLineSuccess() {
         System.out.println(System.lineSeparator()
-                + Sentence.INFO.message + "지하철 노선이 등록되었습니다.");
+                + INFO + "지하철 노선이 등록되었습니다.");
     }
 
     public void printDeleteLineSuccess() {
         System.out.println(System.lineSeparator()
-                + Sentence.INFO.message + "지하철 노선이 삭제되었습니다.");
+                + INFO + "지하철 노선이 삭제되었습니다.");
     }
 
     public void printLines(List<Line> lines) {
         System.out.println(System.lineSeparator()
-                + Sentence.PREFIX.message + "노선 목록");
+                + PREFIX + "노선 목록");
         lines.forEach(line -> {
             String sentence = formatLine(line);
             System.out.println(sentence);
@@ -95,41 +101,41 @@ public class OutputView {
 
     public void printSectionManagement() {
         System.out.println(System.lineSeparator()
-                + Sentence.PREFIX.message + "구간 관리 화면");
-        System.out.println(SectionFeature.SELECT_ONE.message + ". 구간 등록");
-        System.out.println(SectionFeature.SELECT_TWO.message + ". 구간 삭제");
-        System.out.println(SectionFeature.BACK.message + ". 돌아가기" + System.lineSeparator());
+                + PREFIX + "구간 관리 화면");
+        System.out.println(SectionFeature.SELECT_ONE.toString() + ". 구간 등록");
+        System.out.println(SectionFeature.SELECT_TWO.toString() + ". 구간 삭제");
+        System.out.println(SectionFeature.BACK.toString() + ". 돌아가기" + System.lineSeparator());
     }
 
     public void printEnrollSectionSuccess() {
         System.out.println(System.lineSeparator()
-                + Sentence.INFO.message + "구간이 등록되었습니다.");
+                + INFO + "구간이 등록되었습니다.");
     }
 
     public void printDeleteSectionSuccess() {
         System.out.println(System.lineSeparator()
-                + Sentence.INFO.message + "구간이 삭제되었습니다.");
+                + INFO + "구간이 삭제되었습니다.");
     }
 
 
     private String formatStation(Station station) {
-        return Sentence.INFO.message + station.getName();
+        return INFO + station.getName();
     }
 
     private String formatLine(Line line) {
-        return Sentence.INFO.message + line.getName();
+        return INFO + line.getName();
     }
 
     private String formatLineAndStations(Line line) {
         String stationsInfo = line.getStations().stream()
-                .map(station -> Sentence.INFO.message + station.getName())
+                .map(station -> INFO + station.getName())
                 .reduce((s1, s2) -> s1 + System.lineSeparator() + s2)
                 .orElse("");
 
         return String.format("%s%s%n%s---%n%s",
-                Sentence.INFO.message,
+                INFO,
                 line.getName(),
-                Sentence.INFO.message,
+                INFO,
                 stationsInfo);
     }
 }
