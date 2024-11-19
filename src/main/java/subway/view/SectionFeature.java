e15d76041ec13a09d96ac04d672a9927d0d2150b
IM-GYURI <kgulr0517@gmail.com>
2024-11-19 13:54:15 +0900

    feat : 구간 관리 선택지를 enum SectionFeature으로 관리한다.

---
+++ src/main/java/subway/view/SectionFeature.java
@@ -0,0 +1,29 @@
+package subway.view;
+
+import subway.exception.ErrorMessage;
+
+public enum SectionFeature {
+    SELECT_ONE("1"),
+    SELECT_TWO("2"),
+    BACK("B");
+
+    final String message;
+
+    SectionFeature(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+
+    public static SectionFeature getFeatureFromInput(String input) {
+        for (SectionFeature feature : SectionFeature.values()) {
+            if (feature.toString().equalsIgnoreCase(input)) {
+                return feature;
+            }
+        }
+        throw new IllegalArgumentException(ErrorMessage.INVALID_FEATURE.toString());
+    }
+}
