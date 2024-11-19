6121177be361de34d9775fd1bac1f281daa9ba79
IM-GYURI <kgulr0517@gmail.com>
2024-11-19 23:26:18 +0900

    refactor : PREFIX를 상수로 만들어 사용하도록 수정한다.

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class InputView {
 
+    private static final String PREFIX = "## ";
     private final Scanner scanner;
 
     public InputView(Scanner scanner) {
@@ -11,73 +12,73 @@ public class InputView {
     }
 
     public String askFeature() {
-        System.out.println(Sentence.PREFIX.message + "원하는 기능을 선택하세요.");
+        System.out.println(PREFIX + "원하는 기능을 선택하세요.");
         return scanner.nextLine().trim();
     }
 
     public String askStationEnroll() {
         System.out.println(System.lineSeparator()
-                + Sentence.PREFIX.message + "등록할 역 이름을 입력하세요.");
+                + PREFIX + "등록할 역 이름을 입력하세요.");
         return scanner.nextLine().trim();
     }
 
     public String askStationDelete() {
         System.out.println(System.lineSeparator()
-                + Sentence.PREFIX.message + "삭제할 역 이름을 입력하세요.");
+                + PREFIX + "삭제할 역 이름을 입력하세요.");
         return scanner.nextLine().trim();
     }
 
     public String askLineEnroll() {
         System.out.println(System.lineSeparator()
-                + Sentence.PREFIX.message + "등록할 노선 이름을 입력하세요.");
+                + PREFIX + "등록할 노선 이름을 입력하세요.");
         return scanner.nextLine().trim();
     }
 
     public String askUpperStation() {
         System.out.println(System.lineSeparator()
-                + Sentence.PREFIX.message + "등록할 노선의 상행 종점역 이름을 입력하세요.");
+                + PREFIX + "등록할 노선의 상행 종점역 이름을 입력하세요.");
         return scanner.nextLine().trim();
     }
 
     public String askLowerStation() {
         System.out.println(System.lineSeparator()
-                + Sentence.PREFIX.message + "등록할 노선의 하행 종점역 이름을 입력하세요.");
+                + PREFIX + "등록할 노선의 하행 종점역 이름을 입력하세요.");
         return scanner.nextLine().trim();
     }
 
     public String askLineDelete() {
         System.out.println(System.lineSeparator()
-                + Sentence.PREFIX.message + "삭제할 노선 이름을 입력하세요.");
+                + PREFIX + "삭제할 노선 이름을 입력하세요.");
         return scanner.nextLine().trim();
     }
 
     public String askLineNameOfSection() {
         System.out.println(System.lineSeparator()
-                + Sentence.PREFIX.message + "노선을 입력하세요.");
+                + PREFIX + "노선을 입력하세요.");
         return scanner.nextLine().trim();
     }
 
     public String askStationNameOfSection() {
         System.out.println(System.lineSeparator()
-                + Sentence.PREFIX.message + "역이름을 입력하세요.");
+                + PREFIX + "역이름을 입력하세요.");
         return scanner.nextLine().trim();
     }
 
     public String askOrderOfSection() {
         System.out.println(System.lineSeparator()
-                + Sentence.PREFIX.message + "순서를 입력하세요.");
+                + PREFIX + "순서를 입력하세요.");
         return scanner.nextLine().trim();
     }
 
     public String askDeleteLineOfSection() {
         System.out.println(System.lineSeparator()
-                + Sentence.PREFIX.message + "삭제할 구간의 노선을 입력하세요.");
+                + PREFIX + "삭제할 구간의 노선을 입력하세요.");
         return scanner.nextLine().trim();
     }
 
     public String askDeleteStationOfSection() {
         System.out.println(System.lineSeparator()
-                + Sentence.PREFIX.message + "삭제할 구간의 역을 입력하세요.");
+                + PREFIX + "삭제할 구간의 역을 입력하세요.");
         return scanner.nextLine().trim();
     }
 }
