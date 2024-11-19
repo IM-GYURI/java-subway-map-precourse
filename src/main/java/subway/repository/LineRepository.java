a2d7925ff353e8e4d8aeb6135e2057350d8daf35
IM-GYURI <kgulr0517@gmail.com>
2024-11-19 13:50:38 +0900

    feat : LineRepository에 구간 삽입 및 삭제 로직을 구현한다.
    - addStation() : 해당 순서에 역을 삽입
    - deleteSection() : 해당 구간 삭제

--- src/main/java/subway/repository/LineRepository.java
+++ src/main/java/subway/repository/LineRepository.java
@@ -2,10 +2,12 @@ package subway.repository;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import subway.domain.Line;
+import subway.domain.Station;
 import subway.exception.ErrorMessage;
 
 public class LineRepository {
@@ -31,10 +33,48 @@ public class LineRepository {
         lines.removeIf(line -> Objects.equals(line.getName(), name));
     }
 
+    public static void addStation(Line line, Station station, int order) {
+        if (containsStation(line, station)) {
+            throw new IllegalArgumentException(ErrorMessage.LINE_ALREADY_HAS_THIS_STATION.toString());
+        }
+
+        LinkedList<Station> stations = new LinkedList<>(line.getStations());
+        stations.add(order - 1, station);
+
+        deleteLineByName(line.getName());
+
+        Line newLine = new Line(line.getName(), stations);
+        lines.add(newLine);
+    }
+
+    public static void deleteSection(Line line, Station station) {
+        if (!containsStation(line, station)) {
+            throw new IllegalArgumentException(ErrorMessage.LINE_ALREADY_HAS_THIS_STATION.toString());
+        }
+
+        LinkedList<Station> stations = new LinkedList<>(line.getStations());
+
+        if (stations.size() < 3) {
+            throw new IllegalArgumentException(ErrorMessage.STATIONS_SHORTAGE.toString());
+        }
+
+        stations.remove(station);
+
+        deleteLineByName(line.getName());
+
+        Line newLine = new Line(line.getName(), stations);
+        lines.add(newLine);
+    }
+
     public static Optional<Line> findLine(String name) {
         return lines.stream()
                 .filter(line -> line.getName().equals(name))
                 .findFirst();
+    }
+
+    public static boolean containsStation(Line line, Station station) {
+        return line.getStations()
+                .contains(station);
     }
 
     private static boolean isDuplicate(String name) {
